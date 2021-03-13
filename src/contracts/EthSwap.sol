6cec3bb8218b69d0f8c689c3d906f3db1c50a659
ChristianRoque <croquevaldes@hotmail.com>
2021-03-13 16:12:04 -0500

    finished buy tokens function and testing

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,5 +1,31 @@
 pragma solidity ^0.5.0;
+import "./Token.sol";
 
 contract EthSwap {
     string public name = "EthSwap Instant Exchange";
+    Token public token;
+    //Redemption rate = # tokens they received for 1 ether
+    uint256 public rate = 100;
+
+    event TokenPurchased(
+        address account,
+        address token,
+        uint256 amount,
+        uint256 rate
+    );
+
+    constructor(Token _token) public {
+        token = _token;
+    }
+
+    function buyTokens() public payable {
+        uint256 tokenAmount = msg.value * rate;
+        token.transfer(msg.sender, tokenAmount);
+
+        // Check if ethSwap has enough tokens
+        require(token.balanceOf(address(this)) >= tokenAmount);
+
+        // Emit an event
+        emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
 }
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -10,12 +10,12 @@ function tokens(n) {
   return web3.utils.toWei(n, "ether");
 }
 
-contract("EthSwap", (accounts) => {
+contract("EthSwap", ([deployer, investor]) => {
   let token, ethSwap;
 
   before(async () => {
     token = await Token.new();
-    ethSwap = await EthSwap.new();
+    ethSwap = await EthSwap.new(token.address);
     await token.transfer(ethSwap.address, tokens("1000000"));
   });
 
@@ -36,6 +36,38 @@ contract("EthSwap", (accounts) => {
     it("contract has tokens", async () => {
       let balance = await token.balanceOf(ethSwap.address);
       assert.equal(balance.toString(), tokens("1000000"));
+    });
+  });
+
+  describe("buy tokens()", async () => {
+    let result;
+
+    before(async () => {
+      // Send tokens to investor for the price of 1 ether
+      result = await ethSwap.buyTokens({
+        from: investor,
+        value: web3.utils.toWei("1", "ether"),
+      });
+    });
+
+    it("Check if the investor recieved the tokens exchanged", async () => {
+      let investorBalance = await token.balanceOf(investor);
+      assert.equal(investorBalance.toString(), tokens("100"));
+      // Check if the dapp coin was sent
+
+      let ethSwapBalance;
+      ethSwapBalance = await token.balanceOf(ethSwap.address);
+      assert.equal(ethSwapBalance.toString(), tokens("999900"));
+      // Check if the ether was recieved
+
+      ethSwapBalance = await web3.eth.getBalance(ethSwap.address);
+      assert.equal(ethSwapBalance.toString(), tokens("1"));
+      //Check event is working
+      const event = result.logs[0].args;
+      assert.equal(event.account, investor);
+      assert.equal(event.token, token.address);
+      assert.equal(event.amount.toString(), tokens("100").toString());
+      assert.equal(event.rate.toString(), "100");
     });
   });
 });
